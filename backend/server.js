2abb4761e8934663a68afc347d215a4ea983871a
arghjain <arghjain1234@gmail.com>
2026-07-10 20:02:45 +0530

    google ai

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: process.env.FRONTEND_URL,
     },
 });
 
--- backend/services/ai.service.js
+++ backend/services/ai.service.js
@@ -4,7 +4,7 @@ const ApiKey = process.env.GOOGLE_AI_KEY;
 
 const genAI = new GoogleGenerativeAI(ApiKey);
 const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     generationConfig: {
         responseMimeType: "application/json",
         temperature: 0.4,
